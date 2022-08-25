0c2f374afba7d05fce687b22fa285cfb93055e2a
Nikita <nikitax160@yandex.ru>
2022-08-25 13:50:14 +0300

    header added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/img1.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+//const mealsImage = "https://media.eritcom.com/images/business/rest.jpg";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Restoraunt interior" />
+      </div>
+    </>
+  );
+};
+export default Header;
